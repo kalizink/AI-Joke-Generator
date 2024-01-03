65690165396856954176e48671dd94ddd904fd16
kalizink <kalizink65@gmail.com>
2024-01-03 17:04:40 -0600

    AI context

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ function generateJoke() {
 	let generateJoke = document.querySelector("#generate-joke");
 	generateJoke.innerHTML = "Generating a joke for you... please wait";
 	let apiKey = "2ce1e1bf2899t12d0f6obada63d3f945";
-	let context = "please answer in html format";
-	let prompt = "tell me a funny dad joke";
+	let context = "please answer in html format and hide the prompt";
+	let prompt = "what is a funny dad joke";
 	let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 	axios.get(apiUrl).then(showJoke);
 }
