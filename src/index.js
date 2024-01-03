bfe353dac932153a9cfa0c41cc89f8e0381d2bfa
kalizink <kalizink65@gmail.com>
2024-01-03 16:27:41 -0600

    added divs and transition for processing and displaying jokes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 function showJoke() {
-	alert("works");
+	let showJoke = document.querySelector("#display-joke");
+	let generateJoke = document.querySelector("#generate-joke");
+	generateJoke.classList.add("hide");
+	showJoke.innerHTML = "hi";
 }
 
 function generateJoke() {
+	let generateJoke = document.querySelector("#generate-joke");
+	generateJoke.innerHTML = "Generating a joke for you... please wait";
 	let apiKey = "2ce1e1bf2899t12d0f6obada63d3f945";
 	let context = "you a a witty dad";
 	let prompt = "tell me a funny joke about animals";
