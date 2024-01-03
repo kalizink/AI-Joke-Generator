ed23f4e49e296524dd9a4f6973e5a6c76e1eb30a
kalizink <kalizink65@gmail.com>
2024-01-03 16:49:27 -0600

    Downloaded typewriter CDN and applied typewriter feature to js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ function showJoke(response) {
 	let showJoke = document.querySelector("#display-joke");
 	let generateJoke = document.querySelector("#generate-joke");
 	generateJoke.classList.add("hide");
-	showJoke.innerHTML = response.data.answer;
+	new Typewriter("#display-joke", {
+		strings: response.data.answer,
+		autoStart: true,
+	});
 }
 
 function generateJoke() {
 	let generateJoke = document.querySelector("#generate-joke");
 	generateJoke.innerHTML = "Generating a joke for you... please wait";
 	let apiKey = "2ce1e1bf2899t12d0f6obada63d3f945";
-	let context = "you a a witty dad";
+	let context = "add a line break between the punchline and joke";
 	let prompt = "tell me a funny joke about animals";
 	let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 	axios.get(apiUrl).then(showJoke);
