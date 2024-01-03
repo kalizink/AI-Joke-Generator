e54441f2e13eefe66dbeb8a921a687d54e5b85d5
kalizink <kalizink65@gmail.com>
2024-01-03 16:13:34 -0600

    Downloaded axios and generate jokes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-function generateJoke() {
+function showJoke() {
 	alert("works");
 }
 
+function generateJoke() {
+	let apiKey = "2ce1e1bf2899t12d0f6obada63d3f945";
+	let context = "you a a witty dad";
+	let prompt = "tell me a funny joke about animals";
+	let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+	axios.get(apiUrl).then(showJoke);
+}
 let button = document.querySelector("#button");
 button.addEventListener("click", generateJoke);
